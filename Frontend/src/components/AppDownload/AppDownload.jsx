45a95a1b7d74b9dfb83bb9fb711c974493ec563e
farazakram123 <mfarazakram0786@gmail.com>
2025-08-11 21:58:57 +0530

    Add AppDownload component to download our app

---
+++ Frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,15 @@
+import './AppDownload.css'
+import {assets} from '../../assets/assets'
+const AppDownload = () => {
+  return (
+    <div className='app-download'>
+      <p> Experience Better with our <br /> TasteCart App </p>
+      <div className="app-download-img">
+        <img src={assets.play_store} />
+        <img src={assets.app_store} />
+      </div>
+    </div>
+  )
+}
+
+export default AppDownload
