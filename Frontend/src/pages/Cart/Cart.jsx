777fdc4a79a7054c2f32a2f540ecaacae941d755
farazakram123 <mfarazakram0786@gmail.com>
2025-08-12 21:22:31 +0530

    Create PlaceOrder component

--- Frontend/src/pages/Cart/Cart.jsx
+++ Frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,11 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
-import {StoreContext} from '../../context/StoreContext'
+import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
-  const {cartItems, food_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext);
-
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+  const navigate = useNavigate();
   return (
     <div className='cart'>
       <div className="cart-items">
@@ -20,17 +21,17 @@ const Cart = () => {
         <hr />
         {
           food_list.map((foodItem, index) => {
-            if(cartItems[foodItem._id] > 0){
-              return <div key = {index}>
-              <div className='cart-items-title cart-items-item'>
-                <img src={foodItem.image}/>
-                <p>{foodItem.name}</p> 
-                <p>${foodItem.price}</p>
-                <p>{cartItems[foodItem._id]}</p>
-                <p>${foodItem.price*cartItems[foodItem._id]}</p>
-                <p onClick={() => removeFromCart(foodItem._id)} className='cross'>x</p>
-              </div>
-              <hr />
+            if (cartItems[foodItem._id] > 0) {
+              return <div key={index}>
+                <div className='cart-items-title cart-items-item'>
+                  <img src={foodItem.image} />
+                  <p>{foodItem.name}</p>
+                  <p>${foodItem.price}</p>
+                  <p>{cartItems[foodItem._id]}</p>
+                  <p>${foodItem.price * cartItems[foodItem._id]}</p>
+                  <p onClick={() => removeFromCart(foodItem._id)} className='cross'>x</p>
+                </div>
+                <hr />
               </div>
             }
           })
@@ -48,21 +49,22 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount() + 2}</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
 
             </div>
-            <button>Proceed to checkout</button>
+            <button onClick={() => navigate('/order')}>Proceed to checkout</button>
+
           </div>
         </div>
         <div className="cart-promocode">
           <p>If you have a promo code, Enter it here</p>
           <div className="cart-promocode-input">
-            <input type="text" placeholder='promo code'/>
+            <input type="text" placeholder='promo code' />
             <button>Submit</button>
           </div>
         </div>
